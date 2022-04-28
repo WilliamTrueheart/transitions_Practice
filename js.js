49ab032286ed24ca6d7d4f541462f8b431240c8d
Will Newton <91620451+WilliamTrueheart@users.noreply.github.com>
2022-04-28 00:07:50 -0500

    Add files via upload

--- js.js
+++ js.js
@@ -478,3 +478,41 @@ const  selectMoviePoster = (num) => {
 }
 
 
+
+function revealMovies() {
+  var reveals = document.querySelectorAll(".reveal")
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealMovies);
+
+
+function revealAllMusic() {
+  var reveals = document.querySelectorAll(".reveal2")
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealAllMusic);
+
